a24053ce8fb468f9d43b356cb3102d978f7bb3f3
kuremi <kureko@akiratairanoMacBook-ea.local>
2019-12-09 22:59:08 +0900

    react-navogate first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Transition } from 'react-transition-group';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const [inProp, setInProp] = useState(false);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <button onClick={() => setInProp(true)}>switch</button>
+      <div
+        style={{
+          padding: 10,
+        }}
+      >
+        <Transition
+          in={inProp}
+          timeout={200}
+          // mountOnEnter
+          unmountOnExit
+        >
+          {status => {
+            // exited - initial
+            // --- click
+            // entering
+            // entered
+            // --- click
+            // exiting
+            // exited
+            console.log(status);
+            return (
+              <React.Fragment>
+                <div className={['hi', `hi-${status}`].join(' ')}>
+                  <div>
+                    <hi>aa</hi>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          }}
+        </Transition>
+      </div>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
